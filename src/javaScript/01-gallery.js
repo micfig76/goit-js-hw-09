a3167c1d94cb38fc2281a3b2658b1253e9f45729
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-10-07 21:44:48 +0200

    added code to 01-gallery.js

--- src/javaScript/01-gallery.js
+++ src/javaScript/01-gallery.js
@@ -1,5 +1,38 @@
 import SimpleLightbox from "simplelightbox";
 
-const lightbox = new SimpleLightbox('ul.gallery li', {
-    captionType: "alt"
+import { images } from "./images.js";
+
+
+const createImage = ({ preview, original, description }) => {
+    const li = document.createElement("li");
+    li.classList.add("gallery-item");
+
+    li.innerHTML = 
+        `<a class="gallery-link" href="${original}">
+            <img
+                class="gallery-image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>`;
+    return li;
+};
+
+const initImages = (images) => {
+    const imgs = images.map(createImage);
+
+    const fragment = document.createDocumentFragment();
+    fragment.append(...imgs);
+    return fragment;
+};
+
+const main = (root, images) => {
+    document.querySelector(root).appendChild(initImages(images));
+};
+
+main(".gallery", images);
+
+const lightbox = new SimpleLightbox("ul.gallery li", {
+    captionType: "alt",
+    sourceAttr: "href"
 });
