fbd6c541f129e31d81628025372bdb1a975bed79
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-10-09 18:14:27 +0200

    added simplelightbox styling and transferred sode from 01-gallery.js to gallery.js

---
+++ src/javaScript/gallery.js
@@ -0,0 +1,23 @@
+const createImage = ({ preview, original, description }) => {
+    const li = document.createElement("li");
+    li.classList.add("gallery-item");
+
+    li.innerHTML = 
+        `<a class="gallery-link" href="${original}">
+            <img
+                class="gallery-image"
+                src="${preview}"
+                alt="${description}"
+                title="${description}"
+            />
+        </a>`;
+    return li;
+};
+
+export const initImages = (images) => {
+    const imgs = images.map(createImage);
+
+    const fragment = document.createDocumentFragment();
+    fragment.append(...imgs);
+    return fragment;
+};
